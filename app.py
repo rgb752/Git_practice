d1ff6841ee8014199d87d6981f2a0f7974440531
Gaurav <rudrakumargaurav572@gmail.com>
2023-07-25 09:17:58 +0530

    developerA story

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-# hello 
+def addition(a,b):
+    return a+b
+
+def subtraction(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def division(a,b):
+    return a/b
+
+def moduless(a,b):
+    return a//b
